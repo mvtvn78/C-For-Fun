68aada0cbba4655be0d2bc2346526baf11d1dba7
mvtvn78 <mvtvn78@gmail.com>
2024-04-16 14:44:54 +0700

    Change structure

---
+++ HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Advance
+{
+    internal class HelloWorld
+    {
+        static void Main() {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ IO.cs
@@ -0,0 +1,22 @@
+﻿//using System;
+//using System.Net;
+//using System.Net.Mail;
+//class Program
+//{
+   
+//    static void Main()
+//    {
+//        int[] myNumbers = new int[3];
+        
+//        for(int i =0; i< 3; i++)
+//        {
+//            myNumbers[i] = Convert.ToInt32(Console.ReadLine());
+//        }
+//        Array.Sort(myNumbers);
+//        foreach (int i in myNumbers)
+//        {
+//            Console.WriteLine(i);
+//        }
+//    }
+   
+//}
