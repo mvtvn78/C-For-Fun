7c413b3bb0b57fb3eed18dbd0432a8579986e236
mvtvn78 <mvtvn78@gmail.com>
2024-04-16 20:07:39 +0700

    Add Set and get method

---
+++ Interface.cs
@@ -0,0 +1,33 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Mvt
+//{
+//    internal interface Interface1
+//    {
+//        void sound();
+//        void run();
+//    }
+//    interface Interface2
+//    {
+//        bool cute();
+//    }
+//    class Heo : Interface1, Interface2
+//    {
+//        public void sound()
+//        {
+//            Console.WriteLine("ut ut ut");
+//        }
+//        public void run()
+//        {
+//            Console.WriteLine(" ac ac ac");
+//        }
+//        public bool cute()
+//        {
+//            return true;
+//        }
+//    }
+//}
--- Program.cs
+++ Program.cs
@@ -16,11 +16,33 @@ namespace BasicCSharp
             //}
 
             //Interface
-            Heo heo = new Heo();
-            heo.run();
-            heo.sound();
-            Console.WriteLine(heo.cute());
+            //Heo heo = new Heo();
+            //heo.run();
+            //heo.sound();
+            //Console.WriteLine(heo.cute());
 
+            //INTERFACE AND ABSTRACT CAN CREATE ARRAY BUT NOT CREATE OBJECT
+            //Interface1[] interface1 = new Interface1[3];
+            //foreach( Interface1 Interface in interface1 )
+            //{
+            //    Interface.run();
+            //}
+
+            //Setter and getter
+            //Student class1 = new Student(1,"Mai Van Tien",2);
+            //Console.WriteLine(class1.getId);
+            
+            //Readonly and writeonly 
+            Author mvtvn78 = new Author("Mai Van Tien",20);
+            // mvtvn78.name ="Hehe";
+            mvtvn78.Age = 19;
+            mvtvn78.Show();
+
+            //Writeonly
+            //Console.WriteLine(mvtvn78.Age);
+
+            //Lambda
+            Console.WriteLine(mvtvn78.testForEquality(2,1));
         }
     }
 }
