6f48452d977b4561ef1b0154fa367391ff24afd7
mvtvn78 <mvtvn78@gmail.com>
2024-04-16 16:45:20 +0700

    OOP CSharp

--- Loop.cs
+++ Loop.cs
@@ -1,34 +1,35 @@
-﻿using System;
-using System.Text;
-class Mvt
-{
-    static void Main(string[] args)
-    {
-        Console.OutputEncoding = Encoding.UTF8;
-        // For loop
-        for (int i = 1;i<=3000;i++)
-        {
-            Console.WriteLine($"Anh yêu em {i} lần");
-        }
-        // While
-        int j = 1;
-        while(j<=3000)
-        {
-            Console.WriteLine($"Nhớ em {j} lần");
-            j += 1;
-        }
-        // Do - While
-        do
-        {
-           Console.WriteLine($"Anh sẽ block em {j}");
-            j -= 1;
-        }
-        while (j >= 0);
-        //Foreach use for iterable
-        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
-        foreach(int item in arr)
-        {
-            Console.WriteLine($"Phần tử : {item}");
-        }
-    }
-}
+﻿//using System;
+//using System.Text;
+//class Mvt
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.OutputEncoding = Encoding.UTF8;
+//        // For loop
+//        for (int i = 1;i<=3000;i++)
+//        {
+//            Console.WriteLine($"Anh yêu em {i} lần");
+//        }
+//        // While
+//        int j = 1;
+//        while(j<=3000)
+//        {
+//            Console.WriteLine($"Nhớ em {j} lần");
+//            j += 1;
+//        }
+//        // Do - While
+//        do
+//        {
+//           Console.WriteLine($"Anh sẽ block em {j}");
+//            j -= 1;
+//        }
+//        while (j >= 0);
+//        //Foreach use for iterable
+//        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+//        foreach(int item in arr)
+//        {
+//            Console.WriteLine($"Phần tử : {item}");
+//        }
+//        Console.ReadKey(false);
+//    }
+//}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using Mvt;
 namespace BasicCSharp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //OOP
+            Engine[] engines = new Engine[2];
+            engines[0] = new Motobike();
+            engines[1] = new Car();
+            foreach (Engine engine in engines)
+            {
+                engine.Move();
+            }
         }
     }
 }
